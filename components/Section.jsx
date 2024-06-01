c418df14faf4cbfd496a148a02d48c03d2c04b74
vanji <vikashvanchi@gmail.com>
2024-06-01 13:46:03 +0530

    back to swiggy api

--- components/Section.jsx
+++ components/Section.jsx
@@ -22,8 +22,10 @@ const Section = () => {
   if (onlineStatus === false) return <h1>You are offline</h1>;
 
   const fetchData = async () => {
-    const json = data;
-    console.log(data);
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
 
     setlistFilter(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
